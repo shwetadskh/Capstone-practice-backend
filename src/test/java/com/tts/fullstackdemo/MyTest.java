4d96b17381bc2594eab1097af144ff997ad0b5fd
Shweta Deshmukh <79119065+shwetadskh@users.noreply.github.com>
2021-07-20 14:02:52 -0400

    second commit

---
+++ src/test/java/com/tts/fullstackdemo/MyTest.java
@@ -0,0 +1,14 @@
+package com.tts.fullstackdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyTest {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+
+}
